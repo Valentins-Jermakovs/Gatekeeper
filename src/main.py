257edc43f52914e92eb83a02cb85fe8c19243a8f
Valentins-Jermakovs <jermakovsvalentins@gmail.com>
2026-05-31 21:22:10 +0300

    Remove unnecesery code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from starlette.middleware.sessions import SessionMiddleware
-import os
 from dotenv import load_dotenv
 from utils.init_db import init_db
 from routers import auth_route, verify_route
@@ -16,7 +14,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 load_dotenv()
 
-app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 
 app.include_router(auth_route.router)
 app.include_router(verify_route.router)
